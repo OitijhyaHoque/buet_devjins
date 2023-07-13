2933e103b943a82bdde9a7c4cbeae31e79513a65
Md.Tamim Iqbal <113770600+Iqtam@users.noreply.github.com>
2023-07-13 21:30:56 +0600

    Update server.js

--- server.js
+++ server.js
@@ -55,8 +55,10 @@ app.post('/submit', (req, res) => {
   insertData(name, address, n_id);
 
   // Send a response to the client
-  res.send('Data submitted successfully!');
-  res.status(200);
+  res.statusCode=200;
+  res.send({"name": req.body.name, 
+    "address": req.body.address, 
+    "n_id": req.body.n_id, 
 });
 
 // Start the server
