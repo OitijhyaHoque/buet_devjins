3ccd8644abb08f7d2870b4e03b39053440e20237
Iqtam <tamim.iqbal46.21@gmail.com>
2023-07-13 19:20:25 +0600

    tests

--- test/api.test.js
+++ test/api.test.js
@@ -2,17 +2,7 @@ const request = require('supertest');
 const { app, server } = require('../index');
 
 describe('API Routes', () => {
-    it('Hello, world! This is mehrab, this is ci/cd test', async () => {
-        const response = await request(app).get('/hello');
-        expect(response.status).toBe(200);
-        expect(response.text).toBe('Hello, world!');
-    });
-
-    it('should return "Goodbye, world!" on /goodbye route', async () => {
-        const response = await request(app).get('/bye');
-        expect(response.status).toBe(200);
-        expect(response.text).toBe('Goodbye, world!');
-    });
+    
 });
 afterAll((done) => {
     server.close(done);
